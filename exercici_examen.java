e4668de1f2f6796c707f659213860b74db80b1f8
ArnauFerre <101815929+ArnauFerre@users.noreply.github.com>
2022-04-04 16:20:57 +0200

    Update and rename README.md to exercici_examen.java

---
+++ exercici_examen.java
@@ -0,0 +1,9 @@
+public class exercici_examen {
+
+	public static void main(String[] args) {
+		
+		  System.out.println("Soc l'Arnau Ferré Reyes i estic fent l’examen de la UF2 del M01 de DAM");
+
+	}
+
+}
